7227e4713e9c2279f25bce99efae6e5a7c26ddc9
Frédérique Mittelstaedt <git@gfm.io>
2025-11-02 10:58:38 +0000

    fix: always use expand (#12)
    This PR updates the biome config to use `expand: always`. This should prevent release-please from breaking the formatting of the package.json

--- commitlint.config.ts
+++ commitlint.config.ts
@@ -1,15 +1,48 @@
 import type { UserConfig } from '@commitlint/types';
 
 export default {
-  extends: ['@commitlint/config-conventional'],
+  extends: [
+    '@commitlint/config-conventional',
+  ],
   rules: {
-    'scope-empty': [1, 'never'],
-    'scope-enum': [2, 'always', ['config', 'deps', 'ci', 'release']],
-    'subject-case': [2, 'never', ['upper-case', 'pascal-case']],
+    'scope-empty': [
+      1,
+      'never',
+    ],
+    'scope-enum': [
+      2,
+      'always',
+      [
+        'config',
+        'deps',
+        'ci',
+        'release',
+      ],
+    ],
+    'subject-case': [
+      2,
+      'never',
+      [
+        'upper-case',
+        'pascal-case',
+      ],
+    ],
     'type-enum': [
       2,
       'always',
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'perf', 'test', 'build', 'ci', 'chore', 'revert'],
+      [
+        'feat',
+        'fix',
+        'docs',
+        'style',
+        'refactor',
+        'perf',
+        'test',
+        'build',
+        'ci',
+        'chore',
+        'revert',
+      ],
     ],
   },
 } satisfies UserConfig;
